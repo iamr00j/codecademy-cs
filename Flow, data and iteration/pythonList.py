957e6bf71591c47f1a751ca3dceb346b6b5ee182
iamr00j <42660439+iamr00j@users.noreply.github.com>
2019-04-13 23:58:05 +0800

    Create pythonList.py

---
+++ Flow, data and iteration/pythonList.py
@@ -0,0 +1,17 @@
+toppings = ['pepperoni','pineapple','cheese','sausage','olives','anchovies','mushrooms']
+prices = [2,6,1,3,2,7,2]
+num_pizza = len(toppings)
+print("We sell %d different kinds of pizza!" % num_pizza)
+pizzas = list(zip(prices,toppings))
+print (pizzas)
+pizzas.sort()
+print(pizzas)
+cheapest_pizza = pizzas[0]
+print(cheapest_pizza)
+priciest_pizza = pizzas[-1]
+print(priciest_pizza)
+three_cheapest = pizzas[0:3]
+print(three_cheapest)
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
